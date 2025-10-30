8c3ec8600a0edf9683dbb2108efec3d99d2b3ef1
Elegast <elegast.vercruysse@gmail.com>
2025-10-30 16:17:24 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:3L5R4kOlMePehZtt@db.yezkgrihchdjhiypfykc.supabase.co:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
