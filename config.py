461779b3f553cbc284408c303656d96eaff6671d
LennertDC16 <lennerdc.declerck@ugent.be>
2025-11-04 12:17:46 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ class Config:
     SECRET_KEY = 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:3L5R4kOlMePehZtt@db.yezkgrihchdjhiypfykc.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
