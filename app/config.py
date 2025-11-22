ab72d1eb12a51f061fc5a6257cbd9453db2e2309
Elegast <elegast.vercruysse@gmail.com>
2025-11-22 17:57:22 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,3 @@
 class Config:
-    SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.username:3L5R4kOlMePehZtt@localhost:port/dbname'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # ➜ voeg deze toe
     UPLOAD_FOLDER = "app/static/profile_pics"
     ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
