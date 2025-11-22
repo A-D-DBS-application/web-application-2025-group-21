070bf6c2e177d9f868aa09af3945611da5af063a
Elegast <elegast.vercruysse@gmail.com>
2025-11-22 17:53:59 +0100

    profielfoto toevoegen

--- app/__init__.py
+++ app/__init__.py
@@ -2,23 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
-from dotenv import load_dotenv
-import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
-    # laad .env in
     load_dotenv()
 
-    app.config["SECRET_KEY"] = "wachtwoord"
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    # laad configuratie uit Config class
+    from .config import Config
+    app.config.from_object(Config)
 
+    # database
     db.init_app(app)
 
+    # blueprint
     from .routes import main
     app.register_blueprint(main)
 
