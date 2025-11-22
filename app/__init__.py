ac546089e7e46e8e5b78795e9b0f525931790b7a
birthe2507 <Birthe.Dillien@Ugent.be>
2025-11-22 21:41:42 +0100

    flaskBabel

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
+from flask_babel import Babel
+from flask import session
+
 
 db = SQLAlchemy()
+babel = Babel()
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +17,13 @@ def create_app():
     app.config["SECRET_KEY"] = "wachtwoord"
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    #BABEL CONFIG (meertaligheid)
+    app.config["BABEL_DEFAULT_LOCALE"] = "en"
+    app.config["BABEL_SUPPORTED_LOCALES"] = ["en", "nl", "fr"]
+    app.config["BABEL_TRANSLATION_DIRECTORIES"] = "translations"
+
+    babel.init_app(app)
+
 
     db.init_app(app)
 
@@ -20,3 +31,10 @@ def create_app():
     app.register_blueprint(main)
 
     return app
+
+@babel.localeselector
+def get_locale():
+    lang = session.get("language")
+    if lang in ["en", "nl", "fr"]:
+        return lang
+    return "en"
